64b932c46450f0913d432570ceb110968bdb367f
Prittam <prittamraj2018282@gmail.com>
2026-09-15 21:23:07 +0530

    Add backend health check

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,26 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+
 const rideRoutes = require("./routes/ride.routes.js");
 const aiRoutes = require("./routes/ai.routes.js");
-
+const userRoutes = require("./routes/user.routes");
+const captainRoutes = require("./routes/captain.routes");
 
 const app = express();
 
-const userRoutes = require("./routes/user.routes");
-const captainRoutes = require("./routes/captain.routes");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors());
 
 const connectDB = require("./db/db");
-
 connectDB();
+
+app.get("/", (req, res) => {
+  res.status(200).send("RapidAid Backend is Running Successfully!");
+});
+
 app.use("/api/v1/ride",rideRoutes)
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/captain", captainRoutes);
