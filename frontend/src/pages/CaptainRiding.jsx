5327ecd58a4bd8753357e717bbba1720cdb801d1
Prittam <prittamraj2018282@gmail.com>
2026-09-15 21:10:23 +0530

    case error fixed

--- frontend/src/pages/CaptainRiding.jsx
+++ frontend/src/pages/CaptainRiding.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import FinishRide from "../components/FinishRide";
+import FinishRide from "../Components/FinishRide";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import MyMap from "./MyMap";
